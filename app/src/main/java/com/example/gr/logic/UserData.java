e8826b10b1b0ca96abbc491e0482f334fa0c4b23
Santeri <51915262+silsanteri@users.noreply.github.com>
2020-04-20 17:27:16 +0300

    Changed UserData class structure.

--- app/src/main/java/com/example/gr/logic/UserData.java
+++ app/src/main/java/com/example/gr/logic/UserData.java
@@ -1,94 +1,80 @@
 package com.example.gr.logic;
 
-import java.util.ArrayList;
-import java.util.List;
-
 // AUTHOR @silsanteri
 
-public class UserData {
+    public class UserData {
+        private Water water;
+        private Food food;
+        private Exercise exercise;
 
-    // water = List of water added (in milliliters, 1 = 1ml, 100 = 100ml, etc.).
-    // waterAmount = Total amount of water added.
-    private List<Integer> water = new ArrayList<>();
-    private int waterAmount;
-
-    // food = List of calories added (in calories, 1 = 1kcal, 100 = 100kcal, etc.).
-    // foodAmount = Total amount of calories added.
-    private List<Integer> food = new ArrayList<>();
-    private int foodAmount;
-
-    // exercise = List of exercise added (in minutes, 1 = 1min, 100 = 100min, etc.).
-    // exerciseAmount = Total amount of exercise added.
-    private List<Integer> exercise = new ArrayList<>();
-    private int exerciseAmount;
-
-    // CONSTRUCTOR ------------------------------
-    public UserData(){
+        public UserData() {
+        }
     }
 
     // WATER ------------------------------------
-    public void addWater(int amount) {
-        this.water.add(amount);
-        this.waterAmount += amount;
+    class Water {
+        // waterAmount in milliliters, 1 = 1ml, 100 = 100ml, etc.
+        private int waterAmount;
+
+        public Water(){
+            this.waterAmount = 0;
+        }
+
+        public void addWater(int amount) {
+            this.waterAmount += amount;
+        }
+
+        public void removeWater(int amount) {
+            this.waterAmount -= amount;
+        }
+
+        public int returnWaterAmount(){
+            return this.waterAmount;
+        }
     }
 
-    public void removeWater(int index) {
-        this.water.remove(index);
-    }
-
-    public int returnWaterCount(){
-        return this.water.size();
-    }
-
-    public int returnWaterAmount(){
-        return this.waterAmount;
-    }
-
-    public List<Integer> returnWaterArray(){
-        return this.water;
-    }
 
     // FOOD --------------------------------------
-    public void addFood(int amount) {
-        this.food.add(amount);
-        this.foodAmount += amount;
+    class Food {
+        // FoodAmount in calories, 1 = 1kcal, 100 = 100kcal, etc.
+        private int foodAmount;
+
+        public Food(){
+            this.foodAmount = 0;
+        }
+
+        public void addFood(int amount) {
+            this.foodAmount += amount;
+        }
+
+        public void removeFood(int amount) {
+            this.foodAmount -= amount;
+        }
+
+        public int returnFoodAmount(){
+            return this.foodAmount;
+        }
     }
 
-    public void removeFood(int index) {
-        this.food.remove(index);
-    }
-
-    public int returnFoodCount(){
-        return this.food.size();
-    }
-
-    public int returnFoodAmount(){
-        return this.foodAmount;
-    }
-
-    public List<Integer> returnFoodArray(){
-        return this.food;
-    }
 
     // EXERCISE --------------------------------
-    public void addExercise(int amount) {
-        this.exercise.add(amount);
-        this.exerciseAmount += amount;
-    }
+    class Exercise {
+        // exerciseAmount in minutes, 1 = 1min, 100 = 100min, etc.
+        private int exerciseAmount;
 
-    public void removeExercise(int index) {
-        this.exercise.remove(index);
-    }
+        public Exercise(){
+            this.exerciseAmount = 0;
+        }
 
-    public int returnExerciseCount(){
-        return this.exercise.size();
-    }
+        public void addExercise(int amount) {
+            this.exerciseAmount += amount;
+        }
 
-    public int returnExerciseAmount(){
-        return this.exerciseAmount;
-    }
+        public void removeExercise(int amount) {
+            this.exerciseAmount -= amount;
+        }
 
-    public List<Integer> returnExerciseArray(){
-        return this.exercise;
+        public int returnExerciseAmount(){
+            return this.exerciseAmount;
+        }
     }
-}
