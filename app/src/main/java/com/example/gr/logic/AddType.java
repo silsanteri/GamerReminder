f68194da8d7a227c5a4585546b6e520ef869341a
dievskiy <potekhin.ruslan@gmail.com>
2020-04-27 22:37:09 +0300

    dialogs

---
+++ app/src/main/java/com/example/gr/logic/AddType.java
@@ -0,0 +1,11 @@
+package com.example.gr.logic;
+
+/**
+ * Types of possible add items
+ *
+ * @author ruslan
+ * @version 1.0 04/2020
+ */
+public enum AddType {
+    CALORIES, WATER, EXERCISE;
+}
---
+++ app/src/main/java/com/example/gr/logic/DialogHandler.java
@@ -0,0 +1,9 @@
+package com.example.gr.logic;
+
+/**
+ * @author ruslan
+ * @version 1.0 04/2020
+ */
+public interface DialogHandler {
+    void showAddDialog(AddType type);
+}
