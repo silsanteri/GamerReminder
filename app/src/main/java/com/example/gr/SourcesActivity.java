8588d5c90b951e4ee0964ba2a19183a347b7058e
dievskiy <potekhin.ruslan@gmail.com>
2020-05-02 12:26:54 +0300

    SourcesActivity logic added

--- app/src/main/java/com/example/gr/SourcesActivity.java
+++ app/src/main/java/com/example/gr/SourcesActivity.java
@@ -4,18 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SourcesActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sources);
+
+        // add back button and set title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.appbar_title_sources);
+        }
+
+        setUpViews();
+
+    }
+
+    private void setUpViews() {
+        listView = findViewById(R.id.listView);
+        String[] sourcesList = getSources();
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.sources_element, sourcesList) {
+
+        });
+    }
+
+    private String[] getSources() {
+        return getResources().getStringArray(R.array.motivational_source);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            //TODO GO BACK TO SETTINGS INSTEAD OF MAINACTIVITY
             case android.R.id.home:
                 // return to MainActivity if back button pressed
                 onBackPressed();
